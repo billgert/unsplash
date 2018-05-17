a472d8fa3f767fa82372a40781cb0b90499cff4f
billgert <p.billgert@gmail.com>
2018-05-18 00:37:05 +0200

    Improve web service

--- Unsplash/Networking/WebService.swift
+++ Unsplash/Networking/WebService.swift
@@ -7,8 +7,14 @@ class WebService {
             let request = Alamofire.request(router).responseJSON { response in
                 switch response.result {
                 case .success(let json):
-                    let jsonString = try! JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
-                    let decoded = try! JSONDecoder().decode(type, from: jsonString)
+                    guard let jsonString = try? JSONSerialization.data(withJSONObject: json, options: .prettyPrinted) else {
+                        observer.onError(NetworkError.jsonSerialization)
+                        break
+                    }
+                    guard let decoded = try? JSONDecoder().decode(type, from: jsonString) else {
+                        observer.onError(NetworkError.jsonDecoder)
+                        break
+                    }
                     observer.onNext(decoded)
                     observer.onCompleted()
                 case .failure(let error):
