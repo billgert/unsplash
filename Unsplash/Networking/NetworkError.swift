3686d37591ca51101af4e5f127ead042596d08ef
billgert <p.billgert@gmail.com>
2018-05-18 00:37:50 +0200

    Support error handling

--- Unsplash/Networking/NetworkError.swift
+++ Unsplash/Networking/NetworkError.swift
@@ -1,9 +1,21 @@
-//
-//  WebServiceError.swift
-//  Unsplash
-//
-//  Created by Patrik Billgert on 2018-05-17.
-//  Copyright © 2018 Patrik Billgert. All rights reserved.
-//
-
 import Foundation
+
+enum NetworkError: Error {
+    case internet
+    case jsonSerialization
+    case jsonDecoder
+    case backend(errors: [String])
+}
+
+extension NetworkError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case .internet:
+            return "Your internet connection seems to be offline."
+        case .jsonSerialization, .jsonDecoder:
+            return "There seems to be a parsing error."
+        case .backend(let errors):
+            return errors.joined(separator: ". ")
+        }
+    }
+}
