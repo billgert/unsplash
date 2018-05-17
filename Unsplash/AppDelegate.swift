aec88bb0662b5d5900fd6860fe8df6d003d1ae6f
billgert <p.billgert@gmail.com>
2018-05-17 19:16:59 +0200

    Add folders structure

---
+++ Unsplash/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Unsplash
+//
+//  Created by Patrik Billgert on 2018-05-17.
+//  Copyright © 2018 Patrik Billgert. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
