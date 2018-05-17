1713895161e01328e1a74098873d531629138739
billgert <p.billgert@gmail.com>
2018-05-18 00:38:19 +0200

    Test web service from explore

--- Unsplash/Screens/Explore/ExploreViewController.swift
+++ Unsplash/Screens/Explore/ExploreViewController.swift
@@ -1,5 +1,24 @@
 import UIKit
+import RxSwift
 
 class ExploreViewController: UIViewController {
-
+    let webService = WebService()
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Check for reachability and throw error if neccessary
+        webService.request(router: .photos, type: [Photo].self)
+            .subscribe { (event) in
+                switch event {
+                case .next(let photosResponse):
+                    print(photosResponse)
+                case .completed:
+                    print("completed")
+                case .error(let error):
+                    print(error.localizedDescription)
+                }
+            }
+            .disposed(by: disposeBag)
+    }
 }
