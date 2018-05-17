09aff134033c42e36f3ea5f185d6bcc0fef51a57
billgert <p.billgert@gmail.com>
2018-05-17 19:58:49 +0200

    Init root view controller from app delegate

--- Unsplash/AppDelegate.swift
+++ Unsplash/AppDelegate.swift
@@ -13,6 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ExploreViewController()
+        window?.makeKeyAndVisible()
         return true
     }
 }
