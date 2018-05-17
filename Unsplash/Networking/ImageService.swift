3afb532e167613d23a5c6d2f31df731e1fddbb34
billgert <p.billgert@gmail.com>
2018-05-18 00:36:05 +0200

    Add skeleton files

---
+++ Unsplash/Networking/ImageService.swift
@@ -0,0 +1,9 @@
+//
+//  ImageService.swift
+//  Unsplash
+//
+//  Created by Patrik Billgert on 2018-05-18.
+//  Copyright © 2018 Patrik Billgert. All rights reserved.
+//
+
+import Foundation
---
+++ Unsplash/Networking/NetworkError.swift
@@ -0,0 +1,9 @@
+//
+//  WebServiceError.swift
+//  Unsplash
+//
+//  Created by Patrik Billgert on 2018-05-17.
+//  Copyright © 2018 Patrik Billgert. All rights reserved.
+//
+
+import Foundation
