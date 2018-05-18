c1d47210c905bb1ca49e3926cd02f6a569d1e72f
billgert <p.billgert@gmail.com>
2018-05-18 15:07:41 +0200

    Add view controller elements

--- Unsplash/Screens/Photo/PhotoViewController.swift
+++ Unsplash/Screens/Photo/PhotoViewController.swift
@@ -1,15 +1,46 @@
 import UIKit
-import RxSwift
+import AlamofireImage
+import Hero
 
 class PhotoViewController: UIViewController, ErrorHandler {
+    lazy var imageView: UIImageView = {
+        let view = UIImageView()
+        view.contentMode = .scaleAspectFill
+        view.clipsToBounds = true
+        return view
+    }()
+    
     let viewModel: PhotoViewModel
     
     init(viewModel: PhotoViewModel) {
         self.viewModel = viewModel
         super.init(nibName: nil, bundle: nil)
+        modalPresentationStyle = .overFullScreen
+        hero.isEnabled = true
+        hero.modalAnimationType = .zoom
+        loadImage()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .clear
+        
+        view.addSubview(imageView, constraints: [
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    private func loadImage() {
+        guard let imageUrl = viewModel.photo.urlForType(.regular) else { return }
+        guard let url = URL(string: imageUrl) else { return }
+        imageView.hero.id = viewModel.photo.id
+        imageView.af_setImage(withURL: url)
     }
 }
